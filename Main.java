058a1620d8dc7d83cdc7bd72d1d00e9bbed62d70
Rafael Spíndola <rafa.spin95@gmail.com>
2022-11-13 20:15:05 -0300

    Initializing project

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Defining the problem:
+ * Build a basic banking application with simple primary functions like database, deposit and withdraw.
+ * Later, I should add new functions to the older ones as I progress through the development process, like an UI.
+ * Finally, maybe implement SQL databases or APIs.
+ *
+ * Steps:
+ * Create a class responsible for a database
+ * Create a class responsible for depositing money
+ * Create a class responsible for withdrawing money
+ * Use the main method to create the app and execute all functions
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
