f066795acf7c215ad0bdf81a1425a00e366be03f
Rafael Spíndola <rafa.spin95@gmail.com>
2022-11-18 22:19:33 -0300

    Switched comments and checklist inside the code to the README.md file

--- Main.java
+++ Main.java
@@ -1,32 +1,5 @@
 import java.util.Scanner;
 
-/**
- * Defining the problem:
- * Build a basic banking application with simple primary functions like account database, balance, deposit and withdraw.
- * Later, I should add new functions to the older ones as I progress through the development process, like an UI.
- * Finally, maybe implement SQL databases or APIs.
-
- * Steps:
- * Create a class responsible for a database of bank accounts.
- *   (x) Create variable for a name.
- *   (x) Create variable for phone number.
- *   (x) Create variable for e-mail.
- *   (x) Create method to generate a random account number.
- *   (x) Test to see if it's functioning.
-
- * Create a class responsible for transactions generally, including a way to verify balance, to deposit and to withdraw money.
- *   (x) Create a variable and a method to verify balance.
- *   (x) Create a variable and a method to do a deposit.
- *   (x) Create a variable and a method to do a withdrawal.
- *   (x) Test to see if it's functioning.
-
- * Use the main method to create the app and execute all functions.
- *   (x) Create an UI using the terminal
- *   (x) Create a method to execute the function of balance.
- *   (x) Create a method to execute the function of deposit.
- *   (x) Create a method to execute the function of withdraw.
- */
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
